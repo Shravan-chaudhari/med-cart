61020b6e98fa321d6341668ce1c27de28ba68e9b
Shravan <shravanchaudhari99@gmail.com>
2024-01-16 10:59:25 +0530

    header

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,8 +1,12 @@
 "use client";
 import React from "react";
-
+import SearchIcon from "@mui/icons-material/Search";
 import { UserAuth } from "../context/AuthContext";
 import Link from "next/link";
+import "../../styles/navbar.css";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import WatchLaterIcon from "@mui/icons-material/WatchLater";
+import Avatar from "@mui/material/Avatar";
 
 const Navbar = () => {
   const { user, googleSignIn, logOut } = UserAuth();
@@ -21,14 +25,62 @@ const Navbar = () => {
       console.log(error);
     }
   };
-
+  console.log(user);
   return (
     <div className="Navbar">
+      <ul className="Navigation">
+        <li>
+          <div className="tittle">
+            <p>MEd CArt</p>
+          </div>
+        </li>
+        <li>
+          <div className="search">
+            <input
+              type="text"
+              placeholder="Search for medicines and health care products"
+            />
+            <div className="searchbtn">
+              <SearchIcon className="searchIcon" />
+              <p>Search</p>
+            </div>
+          </div>
+        </li>
+        {!user ? (
+          <li onClick={handleSignIn}>
+            <div className="login">
+              <p>Login/Signup</p>
+            </div>
+          </li>
+        ) : (
+          <li onClick={handleSignOut}>
+            <div className="login">
+              <Avatar
+                sx={{ width: 24, height: 24 }}
+                className="avatar"
+                src={user.photoURL}
+              />
+              <p>Welcome, {user.displayName}</p>
+            </div>
+          </li>
+        )}
+
+        <li>
+          <div className="cart">
+            <ShoppingCartIcon className="cartIcon" />
+            <p>Cart</p>
+          </div>
+        </li>
+      </ul>
+
       <ul>
-        <Link href="/cart">HI</Link>
-        <li>hello</li>
-        <li onClick={handleSignOut}>tata</li>
-        <li onClick={handleSignIn}>bye</li>
+        <li>
+          <div className="tagline">
+            <WatchLaterIcon />
+            <p>24 hrs available solution for all your medical needs</p>
+          </div>
+          <p></p>
+        </li>
       </ul>
     </div>
   );
